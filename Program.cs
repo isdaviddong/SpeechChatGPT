b0c61b0b6c1d422ce54373a9cd7b6a90da84fb9d
David <twdeveloper@hotmail.com>
2023-04-24 02:44:06 +0800

    v1

---
+++ Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using Azure;
+using Azure.AI.OpenAI;
+using static System.Environment;
+
+class Program
+{
+    // This example requires environment variables named "OPEN_AI_KEY" and "OPEN_AI_ENDPOINT"
+    // Your endpoint should look like the following https://YOUR_OPEN_AI_RESOURCE_NAME.openai.azure.com/
+    static string openAIKey = "____Open__AI___Key___"; 
+    static string openAIEndpoint = "https://YOUR_OPEN_AI_RESOURCE_NAME.openai.azure.com/";
+
+    // Enter the deployment name you chose when you deployed the model.
+    static string engine = "___gpt35___";
+
+    // This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+    static string speechKey = "___speechKey___";
+    static string speechRegion = "___eastasia___";
+
+    // Prompts Azure OpenAI with a request and synthesizes the response.
+    async static Task AskOpenAI(string prompt)
+    {
+        // Ask Azure OpenAI
+        OpenAIClient client = new(new Uri(openAIEndpoint), new AzureKeyCredential(openAIKey));
+        var ChatCompletionsOptions = new ChatCompletionsOptions()
+        {
+            Messages = {
+                    new ChatMessage(ChatRole.System, "你是一個智能助理，你會回答任何問題，像是一個古老的軍師智者。"),
+                    new ChatMessage(ChatRole.User, prompt),
+                    // new ChatMessage(ChatRole.Assistant, "嗯~可呼。汝請發問，吾定當竭力回答~"),
+                    // new ChatMessage(ChatRole.User, "我該如何才在戰場上戰勝呢?"),
+                },
+            MaxTokens = 100,
+        };
+        Response<ChatCompletions> completionsResponse = client.GetChatCompletions(engine, ChatCompletionsOptions);
+        string text = completionsResponse.Value.Choices[0].Message.Content.Trim();
+        Console.WriteLine($"Azure OpenAI 回覆: \n {text}");
+
+        var speechConfig = SpeechConfig.FromSubscription(speechKey, speechRegion);
+        // The language of the voice that speaks.
+        speechConfig.SpeechSynthesisVoiceName = "zh-TW-HsiaoChenNeural";
+        var audioOutputConfig = AudioConfig.FromDefaultSpeakerOutput();
+
+        using (var speechSynthesizer = new SpeechSynthesizer(speechConfig, audioOutputConfig))
+        {
+            var speechSynthesisResult = await speechSynthesizer.SpeakTextAsync(text).ConfigureAwait(true);
+
+            if (speechSynthesisResult.Reason == ResultReason.SynthesizingAudioCompleted)
+            {
+                Console.WriteLine($"Speech synthesized to speaker for text: [{text}]");
+            }
+            else if (speechSynthesisResult.Reason == ResultReason.Canceled)
+            {
+                var cancellationDetails = SpeechSynthesisCancellationDetails.FromResult(speechSynthesisResult);
+                Console.WriteLine($"Speech synthesis canceled: {cancellationDetails.Reason}");
+
+                if (cancellationDetails.Reason == CancellationReason.Error)
+                {
+                    Console.WriteLine($"Error details: {cancellationDetails.ErrorDetails}");
+                }
+            }
+        }
+    }
+
+    // Continuously listens for speech input to recognize and send as text to Azure OpenAI
+    async static Task ChatWithOpenAI()
+    {
+        // Should be the locale for the speaker's language.
+        var speechConfig = SpeechConfig.FromSubscription(speechKey, speechRegion);
+        speechConfig.SpeechRecognitionLanguage = "zh-TW";
+
+        using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+        using var speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+        var conversationEnded = false;
+
+        while (!conversationEnded)
+        {
+            Console.WriteLine("Azure OpenAI is listening. Say 'Stop' or press Ctrl-Z to end the conversation.");
+
+            // Get audio from the microphone and then send it to the TTS service.
+            var speechRecognitionResult = await speechRecognizer.RecognizeOnceAsync();
+
+            switch (speechRecognitionResult.Reason)
+            {
+                case ResultReason.RecognizedSpeech:
+                    if (speechRecognitionResult.Text == "停止")
+                    {
+                        Console.WriteLine("Conversation ended.");
+                        conversationEnded = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Recognized speech: {speechRecognitionResult.Text}");
+                        await AskOpenAI(speechRecognitionResult.Text).ConfigureAwait(true);
+                    }
+                    break;
+                case ResultReason.NoMatch:
+                    Console.WriteLine($"No speech could be recognized: ");
+                    break;
+                case ResultReason.Canceled:
+                    var cancellationDetails = CancellationDetails.FromResult(speechRecognitionResult);
+                    Console.WriteLine($"Speech Recognition canceled: {cancellationDetails.Reason}");
+                    if (cancellationDetails.Reason == CancellationReason.Error)
+                    {
+                        Console.WriteLine($"Error details={cancellationDetails.ErrorDetails}");
+                    }
+                    break;
+            }
+        }
+    }
+
+    async static Task Main(string[] args)
+    {
+        try
+        {
+            await ChatWithOpenAI().ConfigureAwait(true);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
